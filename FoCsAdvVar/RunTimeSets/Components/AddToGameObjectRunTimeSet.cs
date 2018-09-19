b07b19dafb21c24a57ca626e4c80790e004e5f12
JMiles42 <J@jmiles42.com>
2018-09-20 00:03:27 +1000

    Added AdvVaiable code

---
+++ FoCsAdvVar/RunTimeSets/Components/AddToGameObjectRunTimeSet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ForestOfChaosLib.AdvVar.RuntimeRef.Components
+{
+	[AddComponentMenu(FoCsStrings.COMPONENTS_FOLDER + "/AdvVar/RunTime/" + "Add GameObject RunTime Ref")]
+	public class AddToGameObjectRunTimeSet: BaseAddToRunTimeSet<GameObject, GameObjectRunTimeList>
+	{
+		public override GameObject Value => gameObject;
+	}
+}
